29a84677f606f587e3207dbbe0960c2934f68e45
thameena <thameena8@gmail.com>
2025-08-03 18:44:20 +0530

    Initialise new project

---
+++ src/main/java/com/forkthebill/service/ForkTheBillServiceApplication.java
@@ -0,0 +1,12 @@
+package com.forkthebill.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkTheBillServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForkTheBillServiceApplication.class, args);
+    }
+}
